68c0dab4903f26488b4649460b6dc61ef271a608
maycuatroi <maycuatroi@users.noreply.github.com>
2024-09-30 09:40:53 +0000

    ✅ Ready to clone and code.

---
+++ evo_converter/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for evo_converter."""
+
+from evo_converter.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- evo_converter/cli.py
+++ evo_converter/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for evo_converter project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m evo_converter` and `$ evo_converter `.
 
     This is your program's entry point.
 
